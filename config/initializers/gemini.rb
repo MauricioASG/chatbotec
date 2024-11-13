a916ce085676d8310230d2c5ee02e52eace278ed
G12mare <altamiranomare@gmail.com>
2024-11-12 20:31:50 -0600

    Chabotec

---
+++ config/initializers/gemini.rb
@@ -0,0 +1,9 @@
+# config/initializers/gemini.rb
+
+# Cargar la API Key desde el archivo `config/secrets.yml` o la variable de entorno
+if Rails.env.production?
+  GOOGLE_GEMINI_API_KEY = ENV['AIzaSyABuG786b0ktu4ea2GJ-3aTNyWKbfIvGYM']
+else
+  GOOGLE_GEMINI_API_KEY = Rails.application.credentials.google_gemini[:api_key]
+end
+
